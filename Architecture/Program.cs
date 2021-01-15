e5222b8c8ccdc04735c416326804d2955e3a02cc
ricardo <ricardo@hotmail>
2021-01-15 13:09:41 -0600

    Agregar archivos de proyecto.

---
+++ Architecture/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Architecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
